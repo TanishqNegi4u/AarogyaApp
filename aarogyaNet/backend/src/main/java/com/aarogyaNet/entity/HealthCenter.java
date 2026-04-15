d8aa09691670fbaafaf5940f8cc43e937d2dd2fc
TanishqNegi4u <tanishqnegi766854@gmail.com>
2026-04-15 17:45:37 +0530

    Create aarogyaNet/backend/src/main/java/com/aarogyaNet/entity/HealthCenter.java

---
+++ aarogyaNet/backend/src/main/java/com/aarogyaNet/entity/HealthCenter.java
@@ -0,0 +1,30 @@
+package com.aarogyaNet.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "health_centers")
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class HealthCenter {
+    @Id private String id;
+    private String name;
+    private String type;
+    private String address;
+    private String state;
+    private String district;
+    private String village;
+    private String pincode;
+    private String phone;
+    private String email;
+    private Double latitude;
+    private Double longitude;
+    private Integer totalBeds;
+    private Integer availableBeds;
+    private Boolean hasIcu;
+    private Boolean hasBloodBank;
+    private Boolean hasAmbulance;
+    private Integer ambulanceCount;
+    private Boolean is24x7;
+    private Boolean verified;
+}
